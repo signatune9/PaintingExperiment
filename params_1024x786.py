93051770ae1abdffa413118ef2dd93765c80dd25
Garrett <garrettwillis@utexas.edu>
2019-10-04 11:55:32 -0500

    uploading updated paintings/params from sharon

--- params_1024x786.py
+++ params_1024x786.py
@@ -1,7 +1,7 @@
 # 0 for blocked, 1 for interleaved
-CONDITION = 1
+CONDITION = 0
 
-SUBJECT_ID = '000'
+SUBJECT_ID = '001'
 
 # Horizontal and vertical resolution for the physical monitor
 HRES = 1920
@@ -12,12 +12,11 @@ EXPHRES = 1920
 EXPVRES = 1080
 
 # The length of the monitor in centimeters
-SCREENWIDTH = 88.6
+SCREENWIDTH = 33
 
 # Path to the folder containing the images for the experiment. Only needed if the images are in a different folder
 #   than the script
 FILEPATH = ''
 
 # 0 for mouse, 1 for MRI button mapping (876 123), 2 for SDF JKL
-INPUT_MODE = 2
-
+INPUT_MODE = 1
